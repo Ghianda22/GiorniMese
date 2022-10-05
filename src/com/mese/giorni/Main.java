fabdc67a2507efd59cd4c1597435170976a036d9
Valentina <valelore98@gmail.com>
2022-10-05 17:47:24 +0200

    Developed the whole program

--- src/com/mese/giorni/Main.java
+++ src/com/mese/giorni/Main.java
@@ -1,5 +1,48 @@
 package com.mese.giorni;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserire nome mese: ");
+		String month = in.nextLine();
+		if(month.lastIndexOf(" ") != -1) {			
+			month = month.substring(0, month.indexOf(" ")).toLowerCase();
+		}
+		
+		int numOfDays = 0;
+		
+//		switch (month.toLowerCase()) {
+//			case "novembre":
+//			case "aprile":
+//			case "giugno":
+//			case "settembre":
+//				numOfDays = 30;
+//				break;
+//			case "febbraio":
+//				numOfDays = 28;
+//				break;
+//			default:
+//				numOfDays = 31;
+//				break;
+//		}
+		
+		if(month.equals("febbraio")) {
+			numOfDays = 28;
+		}else if(month.equals("novembre") || month.equals("aprile") || month.equals("giugno") || month.equals("settembre")) {
+			numOfDays = 30;
+		}else if(month.equals("gennaio") || month.equals("marzo") || month.equals("maggio") || month.equals("luglio") || month.equals("agosto") || month.equals("ottobre") || month.equals("dicembre")) {
+			numOfDays = 31;
+		}
+		
+		if(numOfDays == 0) {
+			System.out.println("Non e' un mese");
+		}else {			
+			System.out.println("Il mese di " + month + " ha " + numOfDays + " giorni.");
+		}
+		
+	
+	}
 }
